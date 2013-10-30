ba17edc3472d1c4e9d5a150da895824639a9b6e4
jamesjtong <jtong33@gmail.com>
2013-10-30 19:27:38 -0400

    complete agile rails up to chap 7

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'e9c11a9f643c80ae060fe4337d539fa8e2a4e8fbae2918e35f3fb01f801e8b6561a586f302d2c9517db4c7c98d6b9387d4c12b4907d1a75c7624d3c919411837'
