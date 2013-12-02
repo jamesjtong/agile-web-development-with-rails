27cfb820de5280020585883f4d17c0b467c8627a
jamesjtong <jtong33@gmail.com>
2013-12-01 22:45:52 -0500

    finishes chapter 11

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   include CurrentCart
+    before_action :set_cart
     before_action :set_view_count, only: [:index]
   def index
     @products = Product.order(:title)
